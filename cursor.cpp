713326607b0d126c9d1a3575d7eff65b594b2b1b
Ece Aslan <109793753+apolloxsun@users.noreply.github.com>
2024-07-13 00:52:54 +0300

    detects cursor shape

--- cursor.cpp
+++ cursor.cpp
@@ -1,26 +1,3 @@
-/*#include <iostream>
-#include <X11/Xlib.h>
-#include <X11/cursorfont.h>
-#include <X11/Xcursor/Xcursor.h>
-
-
-int main(int argc, char **argv){
-
-    Display *display;
-    Window window;
-    int screen;
-    Cursor cursor;
-
-    display = XOpenDisplay(NULL);
-
-    cursor = XCreateFontCursor(display, XC_hand2);
-    
-    std::cout << "mesaj" << std::endl;
-
-    XcursorImage *image = XcursorShapeLoadCursor(display, cursor);
-    
-    return 0;
-}*/
 #include <X11/Xlib.h>
 #include <X11/extensions/Xfixes.h>
 #include <X11/Xcursor/Xcursor.h>
@@ -30,19 +7,10 @@ int main(int argc, char **argv){
 #include <thread>  
 #include <chrono>
 
-/*std::unordered_map<unsigned int, std::string> cursorNames = {
-    {10, "arrow"},
-    {11, "hand2"},
-    {XC_watch, "watch"},
-    {XC_xterm, "xterm"},
-    // Add more cursor shapes as needed
-};*/
-
 int main() {
     Display *display;
     int screen;
 
-    /* Open connection to X server */
     display = XOpenDisplay(NULL);
     if (display == NULL) {
         std::cerr << "Cannot open display" << std::endl;
@@ -50,31 +18,26 @@ int main() {
     }
 
     screen = DefaultScreen(display);
-
-    /* Query the current cursor image */
-
     
     while(True){
-    XFixesCursorImage *cursorImage = XFixesGetCursorImage(display);
-    if (cursorImage == NULL) {
-        std::cerr << "Failed to get cursor image" << std::endl;
-        XCloseDisplay(display);
-        return 1;
-    }
-    int cursorShape = cursorImage->cursor_serial;
-    std::cout << cursorShape;
-    if(cursorShape == 11){
-        std::cout << "There is a button\n";
-    }
-    else{
-        std::cout << "There is no button\n";
-    }
-        XFree(cursorImage);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        XFixesCursorImage *cursorImage = XFixesGetCursorImage(display);
+        if (cursorImage == NULL) {
+            std::cerr << "Failed to get cursor image" << std::endl;
+            XCloseDisplay(display);
+            return 1;
     }
 
-
-    // Cleanup 
+        int cursorShape = cursorImage->cursor_serial;
+        std::cout << cursorShape;
+            if(cursorShape == 11){
+                std::cout << "There is a button\n";
+            }
+            else{
+                std::cout << "There is no button\n";
+            }
+                XFree(cursorImage);
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     XCloseDisplay(display);
 
